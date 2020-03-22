7af92b7441bd78946d1016e0e05231031f0ce1f4
Mingyang Zheng <zhengzmy@gmail.com>
2020-03-21 19:34:27 -0700

    create a domain entity

--- Domain/Value.cs
+++ Domain/Value.cs
@@ -4,5 +4,7 @@ namespace Domain
 {
     public class Value
     {
+        public int Id { get; set; }   // convention based: property with name Id as primary key
+        public string name { get; set; }
     }
 }
