6f0b208313607326dadc071ffe21b505c6e27519
Mingyang Zheng <zhengzmy@gmail.com>
2020-03-21 23:41:34 -0700

    Seeding data using Entity Framework fluent configuration

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,5 +13,15 @@ namespace Persistence
 
 
         public DbSet<Value> Values { get; set; }  // "Values" is used as table name
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value {Id = 1, Name = "Value 101"},
+                new Value {Id = 2, Name = "Value 102"},
+                new Value {Id = 3, Name = "Value 103"}
+            );
+        }
     }
 }
