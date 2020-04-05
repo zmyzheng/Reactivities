287159e57aae99441b85478fe035a71a1008cb9d
Mingyang Zheng <zhengzmy@gmail.com>
2020-04-04 23:13:12 -0700

    165. Returning the JWT Token on successful login

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -31,9 +32,11 @@ namespace Application.User
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
+            private readonly IJwtGenerator _jwtGenerator;
 
-            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IJwtGenerator jwtGenerator)
             {
+                this._jwtGenerator = jwtGenerator;
                 this._signInManager = signInManager;
                 this._userManager = userManager;
 
@@ -43,7 +46,7 @@ namespace Application.User
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
-                if (user == null) 
+                if (user == null)
                 {
                     throw new RestException(HttpStatusCode.Unauthorized);
                 }
@@ -56,12 +59,12 @@ namespace Application.User
                     return new User
                     {
                         DisplayName = user.DisplayName,
-                        Token = "This will be a token",
+                        Token = _jwtGenerator.CreateToken(user),
                         Username = user.UserName,
                         Image = null
                     };
                 }
-                else 
+                else
                 {
                     throw new RestException(HttpStatusCode.Unauthorized);
                 }
