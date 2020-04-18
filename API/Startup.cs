bee6b12614a149e578c5c63a0a7e27aa9eaf3168
Mingyang Zheng <zhengzmy@gmail.com>
2020-04-18 15:53:43 -0700

    203. Creating a custom Auth policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -72,6 +72,16 @@ namespace API
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
 
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("IsActivityHost", policy => 
+                {
+                    policy.Requirements.Add(new IsHostRequirement());
+                });
+            });
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+            // AddTransient: lifetime of the operation, rather the request
+
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
             // var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
